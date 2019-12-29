26b263fead396170e687b572a6c703b73df8227b
Chris Clarke <C16398141@mydit.ie>
2019-12-29 15:54:13 +0000

    Added fitness and achievement basic activities and removed some floating action buttons

--- app/src/main/java/fyp/c16398141/healthlete/fitness.java
+++ app/src/main/java/fyp/c16398141/healthlete/fitness.java
@@ -1,6 +1,5 @@
 package fyp.c16398141.healthlete;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,19 +9,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.RelativeLayout;
 
-public class food_log extends AppCompatActivity {
+public class fitness extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_food_log);
+        setContentView(R.layout.activity_fitness);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        RelativeLayout layout1 = findViewById(R.id.relative1);
-        layout1.setBackgroundColor(Color.CYAN);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
