d24a9d4427a0c20181d82717833817c27add5a45
Chris Clarke <c16398141@mydit.ie>
2020-03-03 19:25:00 +0000

    more aesthetic table features incorporated

--- app/src/main/java/fyp/c16398141/healthlete/food_log.java
+++ app/src/main/java/fyp/c16398141/healthlete/food_log.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import com.anychart.AnyChart;
@@ -34,6 +35,9 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.widget.TableRow.LayoutParams;
+//import android.widget.RelativeLayout.LayoutParams;
+//import android.widget.LinearLayout.LayoutParams;
 
 public class food_log extends AppCompatActivity {
 
@@ -78,6 +82,14 @@ public class food_log extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onRestart()
+    {
+        super.onRestart();
+        finish();
+        startActivity(getIntent());
+    }
+
     public void init(TableLayout table) {
         ldb.open();
         displayRows(table);
@@ -101,27 +113,48 @@ public class food_log extends AppCompatActivity {
     public void displayRows(final TableLayout table)
     {
         TableRow heading = new TableRow(this);
+        heading.setGravity(Gravity.CENTER);
         int buttonStyle = R.style.Widget_AppCompat_Button_Borderless;
         ImageButton add = new ImageButton(new ContextThemeWrapper(this, buttonStyle));
         add.setId(View.generateViewId());
-        add.setImageResource(R.mipmap.plus_circle);
+        add.setImageResource(R.drawable.plus_circle);
+        add.setBackgroundResource(R.drawable.backgroundstate);
         heading.addView(add);
         TextView tv1 = new TextView(this);
+        tv1.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL);
         tv1.setText(" Name ");
+        tv1.setTypeface(Typeface.DEFAULT_BOLD);
         tv1.setTextColor(Color.BLACK);
-        heading.addView(tv1);
+        tv1.setBackgroundColor(Color.WHITE);
+        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        lp.setMargins(5,5,0,0);
+        //tv1.setLayoutParams(lp);
+        heading.addView(tv1,lp);
         TextView tv2 = new TextView(this);
+        //RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) tv2.getLayoutParams();
+        //layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        //tv2.setLayoutParams(layoutParams);
+        tv2.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL);
         tv2.setText(" Calories ");
+        tv2.setTypeface(Typeface.DEFAULT_BOLD);
         tv2.setTextColor(Color.BLACK);
-        heading.addView(tv2);
+        tv2.setBackgroundColor(Color.WHITE);
+        //tv2.setLayoutParams(lp);
+        heading.addView(tv2,lp);
         TextView tv3 = new TextView(this);
+        tv3.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL);
         tv3.setText(" Carbs ");
+        tv3.setTypeface(Typeface.DEFAULT_BOLD);
         tv3.setTextColor(Color.BLACK);
-        heading.addView(tv3);
+        tv3.setBackgroundColor(Color.WHITE);
+        heading.addView(tv3,lp);
         TextView tv4 = new TextView(this);
+        tv4.setGravity(Gravity.CENTER | Gravity.CENTER_VERTICAL);
         tv4.setText(" Protein ");
+        tv4.setTypeface(Typeface.DEFAULT_BOLD);
         tv4.setTextColor(Color.BLACK);
-        heading.addView(tv4);
+        tv4.setBackgroundColor(Color.WHITE);
+        heading.addView(tv4,lp);
         table.addView(heading);
 
         add.setOnClickListener(new View.OnClickListener() {
@@ -130,6 +163,7 @@ public class food_log extends AppCompatActivity {
 
                 Intent intent = new Intent(food_log.this, food_entry.class);
                 startActivity(intent);
+                //RETURNING FROM TOP OF SCREEN SHOWS CHANGES WHILE BOTTOM BUTTON RETURN DOESN'T REFRESH PARENT
             }
         });
 
@@ -142,32 +176,38 @@ public class food_log extends AppCompatActivity {
             do {
                 i++;
                 TableRow tbrow = new TableRow(this);
+                tbrow.setGravity(Gravity.CENTER);
                 ImageButton minus = new ImageButton(this);
-                minus.setImageResource(R.mipmap.ic_minus);
+                minus.setImageResource(R.drawable.minus_circle);
+                minus.setBackgroundResource(R.drawable.backgroundstate);
                 minus.setId(entries.getInt(0));
                 tbrow.addView(minus);
                 list.add(minus);
                 TextView t2v = new TextView(this);
+                t2v.setBackgroundColor(Color.WHITE);
                 //t2v.setId(i);
                 t2v.setText(entries.getString(1));
                 t2v.setTextColor(Color.BLACK);
                 t2v.setGravity(Gravity.CENTER);
-                tbrow.addView(t2v);
+                tbrow.addView(t2v,lp);
                 TextView t3v = new TextView(this);
                 t3v.setText(Integer.toString(entries.getInt(4)));
+                t3v.setBackgroundColor(Color.WHITE);
                 t3v.setTextColor(Color.BLACK);
                 t3v.setGravity(Gravity.CENTER);
-                tbrow.addView(t3v);
+                tbrow.addView(t3v,lp);
                 TextView t4v = new TextView(this);
                 t4v.setText(Integer.toString(entries.getInt(5)));
+                t4v.setBackgroundColor(Color.WHITE);
                 t4v.setTextColor(Color.BLACK);
                 t4v.setGravity(Gravity.CENTER);
-                tbrow.addView(t4v);
+                tbrow.addView(t4v,lp);
                 TextView t5v = new TextView(this);
                 t5v.setText(Integer.toString(entries.getInt(6)));
+                t5v.setBackgroundColor(Color.WHITE);
                 t5v.setTextColor(Color.BLACK);
                 t5v.setGravity(Gravity.CENTER);
-                tbrow.addView(t5v);
+                tbrow.addView(t5v,lp);
                 table.addView(tbrow);
             } while (entries.moveToNext());
 
