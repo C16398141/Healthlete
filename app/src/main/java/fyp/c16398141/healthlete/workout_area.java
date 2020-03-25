f2efbd176267979032c6142c3b7d870e47cb5a2f
Chris Clarke <c16398141@mydit.ie>
2020-03-25 03:31:54 +0000

    added google places functionality to get details about a place of interest and then implemented a rigorous filtering of the opening times results with error checking included. Some widgets added too

--- app/src/main/java/fyp/c16398141/healthlete/workout_area.java
+++ app/src/main/java/fyp/c16398141/healthlete/workout_area.java
@@ -8,38 +8,100 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.PlaceReport;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Period;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
 
-public class workout_area extends FragmentActivity implements OnMapReadyCallback, LocationListener { //, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.valueOf;
+
+public class workout_area extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleMap.OnPoiClickListener { //, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     Location currentLocation;
+    EditText etext;
+    Button button;
+
+    ArrayList<String> aday = new ArrayList<>();
+    ArrayList<String> atimes = new ArrayList<>();
+    ArrayList<Integer> opening = new ArrayList<>();
+    ArrayList<Integer> closing = new ArrayList<>();
+
     FusedLocationProviderClient fusedLocationProviderClient;
     private static final int request_code = 101;
+
+    private GoogleApi googleApi;
+    //String key = getString(R.string.my_maps_key);
     //GoogleApiClient client;
+
+    // Initialize Places.
+    //Places.initialize(getApplicationContext(), key);
+    // Create a new Places client instance.
+    //PlacesClient placesClient = Places.createClient(this);
+    List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.OPENING_HOURS);
+
+    FetchPlaceRequest request;
+    PlacesClient placesClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_area);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        etext = (EditText) findViewById(R.id.text);
+        button = (Button) findViewById(R.id.button);
+        button.setVisibility(View.INVISIBLE);
+
+        Places.initialize(getApplicationContext(), getString(R.string.my_maps_key));
+        placesClient = Places.createClient(this);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         fetchLastLocation();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void fetchLastLocation() {
@@ -69,7 +131,8 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
         LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
         mMap.addMarker(new MarkerOptions().position(latLng).title("Look where I am"));
         mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        mMap.setOnPoiClickListener(this);
     }
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
@@ -82,11 +145,129 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    public void onLocationChanged(Location location) {
+    @Override
+    public void onPoiClick(PointOfInterest poi) {
+        Toast.makeText(getApplicationContext(), poi.name + "\nPlace ID:" + poi.placeId + "\nLatitude:" + poi.latLng.latitude + " Longitude:" + poi.latLng.longitude, Toast.LENGTH_SHORT).show();
+
+        request = FetchPlaceRequest.newInstance(poi.placeId, placeFields);
+
+        placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
+            Place place = response.getPlace();
+            try {
+                List<String> hours = place.getOpeningHours().getWeekdayText();
+                Log.i("Opening Times", "Weekday Text Found" + place.getName() + " " + hours);
+
+                // convert from default abstract arraylist
+                Collection<String> times = hours;
+                Log.i("Collection", "Weekday Text Found" + place.getName() + " " + times);
+                String always = "Open 24 hours";
+                String never = "Closed";
+                Pattern pday = Pattern.compile(":");
+                Pattern ptimes = Pattern.compile("–");
+                Pattern pm = Pattern.compile("PM");
+
+                for (String daytime : times){
+                    String type, time_brackets, opening_time, closing_time;
+
+                    Matcher matcher = pday.matcher(daytime);
+                    if (matcher.find()) {
+                        if (daytime.contains(always)){
+                            type = "Open";
+                            aday.add(daytime.substring(0, matcher.start()));
+                            atimes.add(type);
+                        }
+                        else if (daytime.contains(never)){
+                            type = "Closed";
+                            aday.add(daytime.substring(0, matcher.start()));
+                            atimes.add(type);
+                        }
+                        else{
+                            type = "Periodic";
+                                aday.add(daytime.substring(0, matcher.start()));
+                                atimes.add(type);
+                                time_brackets = (daytime.substring(matcher.end()));
+
+                                String[] unwantedChars = {":","\\s"};
+                                for (String s : unwantedChars){
+                                    time_brackets = time_brackets.replaceAll(s,"");
+                                }
+
+                                Matcher matcher2 = ptimes.matcher(time_brackets);
+                                if (matcher2.find()) {
+                                    opening_time = (time_brackets.substring(0, matcher2.start()));
+                                    closing_time = (time_brackets.substring(matcher2.end()));
+
+                                    Matcher matcher3 = pm.matcher(opening_time);
+                                    if(matcher3.find()){
+                                        opening_time = opening_time.replaceAll("PM","");
+                                        opening.add((Integer.parseInt(opening_time))+1200);
+                                    }
+                                    else{
+                                        opening_time = opening_time.replaceAll("AM","");
+                                        opening.add(Integer.parseInt(opening_time));
+                                    }
+
+                                    Matcher matcher4 = pm.matcher(closing_time);
+                                    if(matcher4.find()){
+                                        closing_time = closing_time.replaceAll("PM","");
+                                        closing.add((Integer.parseInt(closing_time))+1200);
+                                    }
+                                    else{
+                                        closing_time = closing_time.replaceAll("AM","");
+                                        closing.add(Integer.parseInt(closing_time));
+                                    }
+
+                                }
+                        }
+                        }
+
+                    }
+                int x = 0;
+                for(int i = 0; i<7; i++){
+                    Log.i(aday.get(i),atimes.get(i));
+                    if(atimes.get(i).equals("Periodic")){
+                        Log.i("Opening",valueOf(opening.get(x)));
+                        Log.i("Closing",valueOf(closing.get(x)));
+                        x++;
+                    }
+                }
+
+                aday.clear();
+                atimes.clear();
+
+                etext.setText(place.getName());
+                button.setVisibility(View.VISIBLE);
+            } catch(NullPointerException e) {
+                Log.i("Opening Times", place.getName() + " opening Times not available");
+            }
+
+            /*if (place.getOpeningHours().getWeekdayText() != null){
+                List<String> hours = place.getOpeningHours().getWeekdayText();
+                Log.i("TAG", "Place found: " + place.getName() + " " + hours);
+            }
+            else if(place.getOpeningHours().getPeriods() != null){
+                List<Period> periods = place.getOpeningHours().getPeriods();
+                Log.i("TAG", "Place found: " + place.getName() + " " + periods);
+            }
+            else{
+                Log.i("TAG", "Place found: " + place.getName() + " Opening Times not available");
+            }*/
+        }).addOnFailureListener((exception) -> {
+            if (exception instanceof ApiException) {
+                ApiException apiException = (ApiException) exception;
+                int statusCode = apiException.getStatusCode();
+                // Handle error with given status code.
+                Log.e("TAG", "Place not found: " + exception.getMessage());
+            }
+        });
+    }
+
+
+   public void onLocationChanged(Location location) {
 
     }
 
-    public void onConnected(@Nullable Bundle bundle){
+    /*public void onConnected(@Nullable Bundle bundle){
 
     }
 
@@ -96,5 +277,7 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
 
     public void onConnectionSuspended(int i){
 
-    }
+    }*/
 }
+
+
