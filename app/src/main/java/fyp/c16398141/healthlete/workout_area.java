7b5ffaf8b86e8b51fc67e04601801a27d49ab79c
Chris Clarke <c16398141@mydit.ie>
2020-03-26 07:14:40 +0000

    added predictive search bar for places, fixed the null error from receiving place latlng attributes, added map camera movement on place selection and markers, added home button functionality

--- app/src/main/java/fyp/c16398141/healthlete/workout_area.java
+++ app/src/main/java/fyp/c16398141/healthlete/workout_area.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
@@ -22,6 +23,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -30,6 +32,9 @@ import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -39,12 +44,12 @@ import java.util.regex.Pattern;
 
 import static java.lang.String.valueOf;
 
+// TODO Handle multiple opening times (closed for lunch), add programmatic layout params for hidden button and fragment, update db to take area location
 public class workout_area extends FragmentActivity implements OnMapReadyCallback, LocationListener, GoogleMap.OnPoiClickListener { //, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
     Location currentLocation;
-    EditText etext;
-    Button button;
+    Button button, home;
 
     LocalDB ldb;
 
@@ -63,7 +68,7 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleApi googleApi;
 
-    List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.OPENING_HOURS);
+    List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.OPENING_HOURS, Place.Field.LAT_LNG);
     FetchPlaceRequest request;
     PlacesClient placesClient;
 
@@ -73,49 +78,82 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_workout_area);
         ldb = new LocalDB(this);
 
-        etext = (EditText) findViewById(R.id.text);
         button = (Button) findViewById(R.id.button);
         button.setVisibility(View.INVISIBLE);
+
+        home = (Button) findViewById(R.id.home);
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+                Toast.makeText(getApplicationContext(), "Place Selected", Toast.LENGTH_SHORT).show();
+
+                request = FetchPlaceRequest.newInstance(place.getId(), placeFields);
+
+                placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
+                    Place filtered_place = response.getPlace();
+
+                    processPlaceDetails(filtered_place);
+
+                });
+            }
+
+            @Override
+            public void onError(Status status) {
+
+                Log.i("TAG", "An error occurred: " + status);
+            }
+        });
 
         Places.initialize(getApplicationContext(), getString(R.string.my_maps_key));
         placesClient = Places.createClient(this);
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         fetchLastLocation();
 
-        button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(v -> {
+
+                ldb.open();
+                long update = ldb.addWorkoutArea(place_id, name, times, user_id);
+
+                if (update == -1) {
+                    Toast.makeText(getApplicationContext(), "Unsuccessful area insert", Toast.LENGTH_SHORT).show();
+                } else if (times == 1){
+                    int area_id = (int) update;
+                    int inserts = 0;
+                    for (int i = 0; i<aday.size(); i++){
+                        boolean result = ldb.addWorkoutAvailability(aday.get(i), atimes.get(i), opening.get(i), closing.get(i), area_id);
+                        if (result == true){
+                            inserts++;
+                        }
+                    }
+                    if (inserts == 0)
+                    {
+                        Toast.makeText(getApplicationContext(), "Unsuccessful inserts", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (inserts == aday.size())
+                    {
+                        Toast.makeText(getApplicationContext(), "Successful inserts", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Partial opening time insert with area insert ", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Successful area insert with opening times not available", Toast.LENGTH_SHORT).show();
+                }
+                ldb.close();
+            });
+
+        home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                    ldb.open();
-                    long update = ldb.addWorkoutArea(place_id, name, times, user_id);
-
-                    if (update == -1) {
-                        Toast.makeText(getApplicationContext(), "Unsuccessful area insert", Toast.LENGTH_SHORT).show();
-                    } else if (times == 1){
-                        int area_id = (int) update;
-                        int inserts = 0;
-                        for (int i = 0; i<aday.size(); i++){
-                            boolean result = ldb.addWorkoutAvailability(aday.get(i), atimes.get(i), opening.get(i), closing.get(i), area_id);
-                            if (result == true){
-                                inserts++;
-                            }
-                        }
-                        if (inserts == 0)
-                        {
-                            Toast.makeText(getApplicationContext(), "Unsuccessful inserts", Toast.LENGTH_SHORT).show();
-                        }
-                        else if (inserts == aday.size())
-                        {
-                            Toast.makeText(getApplicationContext(), "Successful inserts", Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(getApplicationContext(), "Partial opening time insert with area insert ", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Successful area insert with opening times not available", Toast.LENGTH_SHORT).show();
-                    }
-                    ldb.close();
-                }
+                updateMap("Current Location",currentLocation.getLatitude(),currentLocation.getLongitude());
+            }
         });
     }
 
@@ -126,17 +164,14 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>(){
-           @Override
-           public void onSuccess(Location location) {
-               if (location != null){
-                   currentLocation = location;
-                   Toast.makeText(getApplicationContext(),currentLocation.getLatitude() + "" +currentLocation.getLongitude(),Toast.LENGTH_SHORT).show();
-                   SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                           .findFragmentById(R.id.map);
-                   mapFragment.getMapAsync(workout_area.this);
-               }
-           }
+        task.addOnSuccessListener(location -> {
+            if (location != null){
+                currentLocation = location;
+                Toast.makeText(getApplicationContext(),currentLocation.getLatitude() + "" +currentLocation.getLongitude(),Toast.LENGTH_SHORT).show();
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(workout_area.this);
+            }
         });
     }
     @Override
@@ -148,6 +183,15 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
         mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
         mMap.setOnPoiClickListener(this);
+    }
+
+    public void updateMap(String name, double lat, double lng){
+        mMap.clear();
+        LatLng latLng = new LatLng(lat,lng);
+        Marker marker = mMap.addMarker(new MarkerOptions().position(latLng).title(name));
+        marker.showInfoWindow();
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
     }
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
@@ -174,97 +218,7 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
 
         placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
             Place place = response.getPlace();
-            place_id = place.getId();
-            name = place.getName();
-            try {
-                List<String> hours = place.getOpeningHours().getWeekdayText();
-                Log.i("Opening Times", "Found " + name + " " + hours);
-
-                times = 1;
-                // convert from default abstract arraylist
-                Collection<String> times = hours;
-                Log.i("Collection", "Weekday Text Found" + place.getName() + " " + times);
-                String always = "Open 24 hours";
-                String never = "Closed";
-                Pattern pday = Pattern.compile(":");
-                Pattern ptimes = Pattern.compile("–");
-                Pattern pm = Pattern.compile("PM");
-
-                for (String daytime : times){
-                    String type, time_brackets, opening_time, closing_time;
-
-                    Matcher matcher = pday.matcher(daytime);
-                    if (matcher.find()) {
-                        if (daytime.contains(always)){
-                            type = "Open";
-                            int open = 1;
-                            aday.add(daytime.substring(0, matcher.start()));
-                            atimes.add(type);
-                            opening.add(open);
-                            closing.add(open);
-                        }
-                        else if (daytime.contains(never)){
-                            type = "Closed";
-                            int closed = 0;
-                            aday.add(daytime.substring(0, matcher.start()));
-                            atimes.add(type);
-                            opening.add(closed);
-                            closing.add(closed);
-                        }
-                        else{
-                            type = "Periodic";
-                                aday.add(daytime.substring(0, matcher.start()));
-                                atimes.add(type);
-                                time_brackets = (daytime.substring(matcher.end()));
-
-                                String[] unwantedChars = {":","\\s"};
-                                for (String s : unwantedChars){
-                                    time_brackets = time_brackets.replaceAll(s,"");
-                                }
-
-                                Matcher matcher2 = ptimes.matcher(time_brackets);
-                                if (matcher2.find()) {
-                                    opening_time = (time_brackets.substring(0, matcher2.start()));
-                                    closing_time = (time_brackets.substring(matcher2.end()));
-
-                                    Matcher matcher3 = pm.matcher(opening_time);
-                                    if(matcher3.find()){
-                                        opening_time = opening_time.replaceAll("PM","");
-                                        opening.add((Integer.parseInt(opening_time))+1200);
-                                    }
-                                    else{
-                                        opening_time = opening_time.replaceAll("AM","");
-                                        opening.add(Integer.parseInt(opening_time));
-                                    }
-
-                                    Matcher matcher4 = pm.matcher(closing_time);
-                                    if(matcher4.find()){
-                                        closing_time = closing_time.replaceAll("PM","");
-                                        closing.add((Integer.parseInt(closing_time))+1200);
-                                    }
-                                    else{
-                                        closing_time = closing_time.replaceAll("AM","");
-                                        closing.add(Integer.parseInt(closing_time));
-                                    }
-
-                                }
-                        }
-                        }
-
-                    }
-
-                for(int i = 0; i<7; i++){
-                    Log.i(aday.get(i),atimes.get(i));
-                    Log.i("Opening",valueOf(opening.get(i)));
-                    Log.i("Closing",valueOf(closing.get(i)));
-                }
-
-                etext.setText(place.getName());
-                button.setVisibility(View.VISIBLE);
-            } catch(NullPointerException e) {
-                Log.i("Opening Times", place.getName() + " opening Times not available");
-                times = 0;
-            }
+            processPlaceDetails(place);
 
         }).addOnFailureListener((exception) -> {
             if (exception instanceof ApiException) {
@@ -276,22 +230,105 @@ public class workout_area extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
+    public void processPlaceDetails(Place place){
+        place_id = place.getId();
+        name = place.getName();
+        double lat = place.getLatLng().latitude;
+        double lng = place.getLatLng().longitude;
+        updateMap(name,lat,lng);
+        try {
+            List<String> hours = place.getOpeningHours().getWeekdayText();
+            Log.i("Opening Times", "Found " + name + " " + hours);
+
+            times = 1;
+            // convert from default abstract arraylist
+            Collection<String> times = hours;
+
+            String always = "Open 24 hours";
+            String never = "Closed";
+            Pattern pday = Pattern.compile(":");
+            Pattern ptimes = Pattern.compile("–");
+            Pattern pm = Pattern.compile("pm");
+
+            for (String daytime : times){
+                String type, time_brackets, opening_time, closing_time;
+
+                Matcher matcher = pday.matcher(daytime);
+                if (matcher.find()) {
+                    if (daytime.contains(always)){
+                        type = "Open";
+                        int open = 1;
+                        aday.add(daytime.substring(0, matcher.start()));
+                        atimes.add(type);
+                        opening.add(open);
+                        closing.add(open);
+                    }
+                    else if (daytime.contains(never)){
+                        type = "Closed";
+                        int closed = 0;
+                        aday.add(daytime.substring(0, matcher.start()));
+                        atimes.add(type);
+                        opening.add(closed);
+                        closing.add(closed);
+                    }
+                    else{
+                        type = "Periodic";
+                        daytime = daytime.toLowerCase();
+                        aday.add(daytime.substring(0, matcher.start()));
+                        atimes.add(type);
+                        time_brackets = (daytime.substring(matcher.end()));
+
+                        String[] unwantedChars = {":","\\s"};
+                        for (String s : unwantedChars){
+                            time_brackets = time_brackets.replaceAll(s,"");
+                        }
+
+                        Matcher matcher2 = ptimes.matcher(time_brackets);
+                        if (matcher2.find()) {
+                            opening_time = (time_brackets.substring(0, matcher2.start()));
+                            closing_time = (time_brackets.substring(matcher2.end()));
+
+                            Matcher matcher3 = pm.matcher(opening_time);
+                            if(matcher3.find()){
+                                opening_time = opening_time.replaceAll("pm","");
+                                opening.add((Integer.parseInt(opening_time))+1200);
+                            }
+                            else{
+                                opening_time = opening_time.replaceAll("am","");
+                                opening.add(Integer.parseInt(opening_time));
+                            }
+
+                            Matcher matcher4 = pm.matcher(closing_time);
+                            if(matcher4.find()){
+                                closing_time = closing_time.replaceAll("pm","");
+                                closing.add((Integer.parseInt(closing_time))+1200);
+                            }
+                            else{
+                                closing_time = closing_time.replaceAll("am","");
+                                closing.add(Integer.parseInt(closing_time));
+                            }
+                        }
+                    }
+                }
+            }
+
+            for(int i = 0; i<7; i++){
+                Log.i(aday.get(i),atimes.get(i));
+                Log.i("Opening",valueOf(opening.get(i)));
+                Log.i("Closing",valueOf(closing.get(i)));
+            }
+        } catch(NullPointerException e) {
+            Log.i("Opening Times", place.getName() + " opening Times not available");
+            times = 0;
+        }
+
+        button.setVisibility(View.VISIBLE);
+    }
 
    public void onLocationChanged(Location location) {
 
     }
 
-    /*public void onConnected(@Nullable Bundle bundle){
-
-    }
-
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult){
-
-    }
-
-    public void onConnectionSuspended(int i){
-
-    }*/
 }
 
 
